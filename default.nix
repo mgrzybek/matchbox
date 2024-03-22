2cf578aa2f1382ed1f4d6372d42ce5a1d85dd1f3
Mathieu.Grzybek <mathieu.grzybek@s3ns.io>
2024-03-22 15:16:32 +0100

    Add Nix buildning configuration

---
+++ default.nix
@@ -0,0 +1,8 @@
+{pkgs ? import <nixpkgs> {}}: let
+  lib = pkgs.lib;
+  matchbox = pkgs.buildGoModule {
+  name = "matchbox";
+  src = lib.cleanSource ../matchbox;
+  vendorHash = "sha256-sVC4xeQIcqAbKU4MOAtNicHcioYjdsleQwKWLstnjfk";
+};
+in matchbox
